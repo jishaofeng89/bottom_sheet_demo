c102f805459327068a550b453d266629554b956e
jishaofeng89 <1195957057@qq.com>
2020-09-28 12:37:27 +0800

    add basic framework

---
+++ lib/bottom_sheet_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetPage extends StatefulWidget {
+  BottomSheetPage({Key key}) : super(key: key);
+
+  @override
+  _BottomSheetPageState createState() => _BottomSheetPageState();
+}
+
+class _BottomSheetPageState extends State<BottomSheetPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('1'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_sheet_demo/bottom_sheet_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,7 +27,8 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BottomSheetPage(),
     );
   }
 }
